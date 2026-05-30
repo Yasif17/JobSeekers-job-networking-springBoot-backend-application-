b3d5f82e589bbe1bc656fba87c2bf8d600f11f21
Yasif khan <125533092+Yasif17@users.noreply.github.com>
2026-05-30 09:34:07 +0530

    Add AcceptConnectionRequestEvent class

---
+++ notification-service/src/main/java/com/yasif/jobseekers/connections_service/event/AcceptConnectionRequestEvent.java
@@ -0,0 +1,9 @@
+package com.yasif.jobseekers.connections_service.event;
+
+import lombok.Data;
+
+@Data
+public class AcceptConnectionRequestEvent {
+    private Long senderId;
+    private Long receiverId;
+}
